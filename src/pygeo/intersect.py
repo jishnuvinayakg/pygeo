690e3817641a8d57cfe1689e7dc41f833b0e7737
Jishnu Vinayak G <jishnuvinayak91@gmail.com>
2020-07-26 19:19:09 +0200

    Corrections and commenting

--- src/pygeo/intersect.py
+++ src/pygeo/intersect.py
@@ -1,4 +1,6 @@
 from .objects import Ray, Sphere, Triangle
+import numpy as np 
+import math as m 
 
 
 def intersect(first_object, second_object):
@@ -6,7 +8,43 @@ def intersect(first_object, second_object):
 
 
 def _intersect_ray_with_sphere(ray, sphere):
-    ...
+
+    if isinstance(ray,Ray) and isinstance(sphere,Sphere):
+
+        intersection_points = []
+
+        ray_origin = ray.origin._point
+        ray_direction = ray.direction._vector
+        sphere_center = sphere.center._point
+        sphere_radius = sphere.radius 
+
+        a = np.dot(ray_direction,ray_direction)
+        b = 2*np.dot(ray_direction,ray_origin-sphere_center)
+        c = np.dot(ray_origin-sphere_center,ray_origin-sphere_center) - sphere_radius**2
+
+        discriminant = b**2 - 4*a*c
+
+        if m.isclose(discriminant,0):
+            t = -(b/2*a)
+            point_1 = ray_origin + t*ray_direction
+            #intersection_points = np.append(intersection_points,point_1.tolist())
+            intersection_points.append(point_1.tolist())
+            return 1,intersection_points
+        
+        elif discriminant > 0:
+            t_0 = (-b + m.sqrt(discriminant))/2*a
+            t_1 = (-b - m.sqrt(discriminant))/2*a
+            point_0 = ray_origin + t_0*ray_direction
+            point_1 = ray_origin + t_1*ray_direction
+            intersection_points.append(point_0.tolist())
+            intersection_points.append(point_1.tolist())
+            print(intersection_points)
+            return 2,intersection_points
+        else:
+            return 0,intersection_points
+    else:
+        return NotImplemented
+
 
 
 def _intersect_ray_with_triangle(ray, triangle):
--- tests/test_intersect.py
+++ tests/test_intersect.py
@@ -1,15 +1,59 @@
-from pygeo.intersect import (
+from src.pygeo.intersect import (
     intersect,
     _intersect_ray_with_sphere,
     _intersect_ray_with_triangle,
 )
-from pygeo.objects import Ray, Sphere, Triangle
+from src.pygeo.objects import Ray, Sphere, Triangle,Point,Vector
+import numpy as np 
 
 
 # intersect
 
 
-# _intersect_ray_with_sphere
+# test _intersect_ray_with_sphere
+def test_ray_sphere_intersection_2_point_return_true():
+    '''Test case : intersection of ray @ 2 points with a sphere. 
+       Sphere : center [0,0,0], radius = 5
+       Ray : Begin [-6,3,0], direction[1,0,0]
+       Expected intersection points : [-4,3,0] & [4,3,0]'''
+    point_1 =[-4,3,0]
+    point_2 =[4,3,0]
+    assert_flag = False
+    ray = Ray(Point((-6,3,0)),Vector((1,0,0)))
+    sphere = Sphere(Point((0,0,0)),5)
+    num_intersection_point,points = _intersect_ray_with_sphere(ray,sphere)
+    if ((points[0] == point_1 or points[0] == point_2) and 
+    (points[1] == point_1 or points[1] == point_2)):
+        assert_flag = True
+    assert(num_intersection_point==2 and assert_flag)is True
+
+def test_ray_sphere_intersection_1_point_return_true():
+    '''Test case : intersection of ray @ 1 point with a sphere. 
+       Sphere : center [0,0,0], radius = 5
+       Ray : Begin [-6,5,0], direction[1,0,0]
+       Expected intersection points : [0,5,0] '''
+    point_3 =[0.0,5.0,0.0]
+    assert_flag = False
+    ray = Ray(Point((-6,5,0)),Vector((1,0,0)))
+    sphere = Sphere(Point((0,0,0)),5)
+    num_intersection_point,points = _intersect_ray_with_sphere(ray,sphere)
+    print(point_3)
+    print(points[0])
+    print(points[0] == point_3)
+    if points[0] == point_3:
+        assert_flag = True
+
+    assert(num_intersection_point==1 and assert_flag)is True
+
+def test_ray_sphere_no_intersection_return_true():
+    '''Test case : intersection of ray @ 0 point with a sphere. 
+       Sphere : center [0,0,0], radius = 5
+       Ray : Begin [-6,6,0], direction[1,0,0]
+       Expected intersection points : No intersection '''
+    ray = Ray(Point((-6,6,0)),Vector((1,0,0)))
+    sphere = Sphere(Point((0,0,0)),5)
+    num_intersection_point,_ = _intersect_ray_with_sphere(ray,sphere)
+    assert(num_intersection_point==0)is True
 
 
 # _intersect_ray_with_triangle
